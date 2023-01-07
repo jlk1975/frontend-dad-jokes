11fbb478f74d1a1611d718f92bb7e645b2009e05
jlk <jkendall1975@gmail.com>
2023-01-07 08:21:51 -0500

    build the biz1

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Jason',
+        'title': 'Blog Post 1',
+        'content': '1st post content',
+        'date_posted': 'Jan 07, 2023'
+    },
+    {
+        'author': 'CMoney',
+        'title': 'Blog Post 2',
+        'content': '2nd post content',
+        'date_posted': 'Jan 06, 2023'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
+
